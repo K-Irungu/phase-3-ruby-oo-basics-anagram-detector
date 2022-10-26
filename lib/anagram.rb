6f4fed3b141ce4ca43ba15f732baf74367e99a18
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-10-26 20:42:07 +0300

    commit tree b3e53fd156df3b6f9e1a85ea1f173dc407f23045

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        anagrams = array.filter { |each| each.chars.sort == word.chars.sort }
+        anagrams[0] ? anagrams : []
+    end
+
+end
+
+a1 = Anagram.new("listen")
+pp a1.match(["enlists", "google", "inlets", "banana"])
